1d303be6725e79dac89f6b21bec07d01b571a50e
thor <thor@THX-1138.Virus>
2019-02-22 17:00:32 -0500

    corrected E0/E1 typo in pins_PICAOLD.h

--- Marlin/pins_PICAOLD.h
+++ Marlin/pins_PICAOLD.h
@@ -79,9 +79,9 @@ AD12 = 66;  AD13 = 67;  AD14 = 68;  AD15 = 69;
  #define E0_DIR_PIN          24
  #define E0_ENABLE_PIN       26
 
- #define E0_STEP_PIN         68 
- #define E0_DIR_PIN          28
- #define E0_ENABLE_PIN       27
+ #define E1_STEP_PIN         68 
+ #define E1_DIR_PIN          28
+ #define E1_ENABLE_PIN       27
 
  #define SDPOWER            -1
  #define LED_PIN            -1 
